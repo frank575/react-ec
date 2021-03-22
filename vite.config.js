38759958124e9f465d6365d4e500de4d29204dc1
吳俊緯 <wcw850710@gmail.com>
2021-03-22 09:04:28 +0800

    fix: require path

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+const path = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
